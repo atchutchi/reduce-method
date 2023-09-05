f1951847515424858cb63e2a559f912490f21911
Atchutchi Ferreira <ferreira.atchutchi@gmail.com>
2023-09-05 04:59:21 +0000

    Added reduce method

--- reduce.js
+++ reduce.js
@@ -7,7 +7,17 @@
 
 // Summing an array of numbers:
 
-
+const nums = [0, 1, 2, 3, 4];
+//let sum = nums.reduce((acc, curr) => acc + curr, 0);
+//console.log(sum);
+let sum = nums.reduce((acc, curr) => {
+  console.log(
+    "Accumulator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+  );
+  return acc + curr;
+  }, 0);
 const teamMembers = [
   {
     name: 'Andrew',
